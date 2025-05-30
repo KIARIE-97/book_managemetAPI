6ffe7e0e5b013563354946e613732470c6a529ee
sarah kiarie <sarahwanjiruki1@gmail.com>
2025-05-30 12:06:28 +0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProfileModule } from './profile/profile.module';
-import { UserModule } from './user/user.module';
-import { BookModule } from './book/book.module';
-import { CategoryModule } from './category/category.module';
-import { BookreviewModule } from './bookreview/bookreview.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
@@ -14,8 +7,20 @@ import { BookreviewsModule } from './bookreviews/bookreviews.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ProfileModule, UserModule, BookModule, CategoryModule, BookreviewModule, ProfilesModule, UsersModule, BooksModule, CategoiesModule, BookreviewsModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ProfilesModule,
+    UsersModule,
+    BooksModule,
+    CategoiesModule,
+    BookreviewsModule,
+    ProfilesModule,
+    UsersModule,
+    BooksModule,
+    CategoiesModule,
+    BookreviewsModule,
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
